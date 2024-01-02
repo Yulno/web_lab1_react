d5047de1be3e9d32d34d43928527c55b3db7859d
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-01-03 01:04:28 +0600

    Слайдер, выравнивание контента по центру

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import Header from "./components/header";
 import ServiceCard from "./components/servicecard";
 import styles from "./App.module.css";
 import {Providers, Mobile} from "./components/infocard";
-import './App.module.css'
+import Slider from "./components/slider";
 
 function App() {
  
   return (
     <>
-      <Header/>
+      <Header className = {styles.header_content}/>
       <ServiceCard className = {styles.margins}/>
       <Providers className = {styles.margins}/>
       <Mobile className = {styles.margins}/>
+      <Slider className = {styles.margins}/>
     </>
   );
 }
---
+++ src/components/slider/slider.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import styles from './slider.module.css';
+import arrowleft from "../../assets/left_arrow.png";
+import arrowright from "../../assets/right_arrow.png";
+import customer from "../../assets/customer.png"
+import dots from "../../assets/dots.png";
+
+interface IProps {
+    className?: string;
+}
+const Slider: React.FC<IProps> = ({className}) => {
+    const reviews = [
+        {
+            name: 'Edward Newgate',
+            position: 'Founder Circle',
+            info: '“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”',
+        },
+        {
+            name: 'John Doe',
+            position: 'CEO',
+            info: '“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”',
+        },
+        {
+            name: 'Alice Johnson',
+            position: 'Head of Operations',
+            info: '“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”',
+        },
+        {
+            name: 'Emily Smith',
+            position: 'Marketing Director',
+            info: '“Our dedicated patient engagement app and web portal allow you to access information instantaneously (no tedeous form, long calls, or administrative hassle) and securely”',
+        }
+    ];
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleSlideChange = (index: number) => {
+        setCurrentSlide(index);
+    };
+
+    return (
+        <>
+            <div className={`${className} ${styles.container}`}>
+                <div className={styles.card}>
+                    <div className={styles.title}>
+                        What our customers are saying
+                    </div>
+                    <div className={styles.line_container}><div className={styles.line}></div></div>
+                    <div className={styles.customer_container}>
+                        <div className={styles.customer}>
+                            <img src={customer} alt="customer" className={styles.customer_photo} />
+                        </div>
+                        <div className={styles.customer_details}>
+                            <div className={styles.name}>
+                                {reviews[currentSlide].name}
+                            </div>
+                            <div className={styles.pos_at_work}>
+                                {reviews[currentSlide].position}
+                            </div>
+                        </div>
+                        <div className={styles.info}>
+                            {reviews[currentSlide].info}
+                        </div>
+                        <img src={dots} alt="dots" className={styles.dots} />
+                        <img src={dots} alt="dots" className={styles.dotsblue} />
+                    </div>
+                </div>
+            </div>
+            <div className={styles.slider_dots}>
+                <div className={styles.modal_nav_buttons}>
+                    <button
+                        className={`${styles.modal_nav_button} ${currentSlide === 0 ? styles.disabled : ''}`}
+                        onClick={() => handleSlideChange(currentSlide - 1)}
+                        disabled={currentSlide === 0}
+                    >
+                        <img src={arrowleft} alt="Previous"/>
+                    </button>
+                    <div className={styles.dot_indicator}>
+                        {reviews.map((_, index) => (
+                            <div
+                                key={index}
+                                className={`${styles.dot} ${index === currentSlide ? styles.active_dot : ''}`}
+                                onClick={() => handleSlideChange(index)}
+                            />
+                        ))}
+                    </div>
+                    <button
+                        className={`${styles.modal_nav_button} ${currentSlide === reviews.length - 1 ? styles.disabled : ''}`}
+                        onClick={() => handleSlideChange(currentSlide + 1)}
+                        disabled={currentSlide === reviews.length - 1}
+                    >
+                        <img src={arrowright} alt="Next"/>
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Slider;
