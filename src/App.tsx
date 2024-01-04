f13d762907cccaa4d32ba5371d6a31fe3a688404
Yuliya Novikova <yuliyanovikowa@gmail.com>
2024-01-04 11:48:41 +0600

    Убрала ограничения по ширине контента

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import ServiceCard from "./components/servicecard";
 import styles from "./App.module.css";
 import {Providers, Mobile} from "./components/infocard";
 import Slider from "./components/slider";
+import Articlecard from "./components/articlecard";
 
 function App() {
  
   return (
     <>
-      <Header className = {styles.header_content}/>
+      <Header/>
       <ServiceCard className = {styles.margins}/>
       <Providers className = {styles.margins}/>
       <Mobile className = {styles.margins}/>
       <Slider className = {styles.margins}/>
+      <Articlecard className = {styles.margins}/>
     </>
   );
 }
